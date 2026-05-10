4ec75891c8160f0503338dfc2b020b4703aab532
Majunior <mycodes774@gmail.com>
2026-05-10 18:11:20 +0530

    checking connection

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Link, Route, Routes, useParams } from 'react-router-dom'
 import type { FileRecord, ShareMeta } from './types';
 
 const API_BASE = import.meta.env.VITE_API_BASE || 'https://goon-valiant-fetal.ngrok-free.dev/api';
-
+const heyy = 'Hello, World!';
 function buildTree(files: FileRecord[]) {
   const tree: Record<string, any> = {};
   files.forEach((file) => {
